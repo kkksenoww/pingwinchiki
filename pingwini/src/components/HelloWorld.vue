5aadb18298bf11b531d9e0d794808c94501adee4
Svetlana-Nilova <snovyj06@gmail.com>
2026-04-22 12:06:56 +0300

    test

--- pingwini/src/components/HelloWorld.vue
+++ pingwini/src/components/HelloWorld.vue
@@ -9,16 +9,7 @@ const count = ref(0)
 
 <template>
   <section id="center">
-    <div class="hero">
-      <img :src="heroImg" class="base" width="170" height="179" alt="" />
-      <img :src="vueLogo" class="framework" alt="Vue logo" />
-      <img :src="viteLogo" class="vite" alt="Vite logo" />
-    </div>
-    <div>
-      <h1>Get started</h1>
-      <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
-    </div>
-    <button class="counter" @click="count++">Count is {{ count }}</button>
+    <h1>Здесь будет симулятор пингвина</h1>
   </section>
 
   <div class="ticks"></div>
